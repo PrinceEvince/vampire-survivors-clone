6c519ca615bea7120e60123fb3f72d1ee866bd91
Wayne <crazyfredsteve@gmail.com>
2025-04-13 12:56:27 -0400

    Big fucking stinky shit log
    Added some temp sprites for player and enemies, added a camera to the player
    Character rotation follows mouse, enemy rotation follows playerr

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,6 +1,6 @@
 extends Node2D  # Or the type of node you're working with
 
-@onready var player = get_node("/root/Game/Player/CharacterBody2D")
+@onready var player = get_node("/root/Game/Player/CharacterBody2D") 
 var SPEED = 50
 
 func _ready():
@@ -9,7 +9,12 @@ func _ready():
 		randf_range(0, 700)
 	)
 	
+	%Sprite2D.frame = randi_range(1,4)
+
+	
 func _process(delta):
 	var direction = (player.global_position - global_position).normalized() # normalized() removes the distance part and just turns it into directions
 	global_position += direction * SPEED * delta
-	
+	var player_position = player.global_position
+	var player_direction = (player_position - global_position).normalized()
+	rotation = player_direction.angle()
--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,6 +1,11 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var parent = get_parent()
+
+func _ready():
+	print(parent)
+
 
 func get_input():
 	var input_direction = Input.get_vector("a", "d", "w", "s")
@@ -9,3 +14,8 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+	var mouse_position = get_global_mouse_position()
+	var direction = (mouse_position - global_position).normalized()
+	rotation = direction.angle()
+	
+	
