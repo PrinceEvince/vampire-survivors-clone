63df50f0810b753aee5a52235f1f3b41dace7134
blpeters67 <126603368+blpeters67@users.noreply.github.com>
2025-04-14 18:20:39 -0400

    started working on stuff which i did not finish and its broken rn

--- scenes/visual_bullet_timer.gd
+++
@@ -1,7 +0,0 @@
-extends TextureProgressBar
-
-func _process(delta: float) -> void:
-	if value <= 0:
-		pass
-	elif value > 0:
-		value -= 1
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 @export var speed = 400
 @onready var parent = get_parent()
-var bullet_scene = preload("res://bullet.tscn")
 @onready var camera = $Camera2D  # Assuming the Camera2D node is a child of the player
 var mouse_position
 var direction
@@ -21,12 +20,4 @@ func _physics_process(delta):
 	mouse_position = get_global_mouse_position()
 	direction = (mouse_position - global_position).normalized()
 	$Sprite2D.rotation = direction.angle()
-
-func _on_gun_timer_timeout() -> void:
-	$VisualBulletTimer.value = 180
-	var shot_bullet = bullet_scene.instantiate()
-	shot_bullet.global_position = global_position
-	shot_bullet.sfx.connect($SFXController.play_sfx)
-	shot_bullet.direction = direction
-	add_sibling(shot_bullet)
-	%GunTimer.start()
+	
