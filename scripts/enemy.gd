ca6d77fddd0ea491f75ac74190aaa8ffeb91dbe8
Wayne <crazyfredsteve@gmail.com>
2025-04-19 12:36:09 -0400

    SAUSAGE

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -9,13 +9,17 @@ func _ready():
 		randf_range(0, 1200),  # change later to only spawn offscreen
 		randf_range(0, 700)
 	)
-	%Sprite2D.frame = randi_range(1,4)
 
 func _physics_process(delta):
 	var direction = (GlobalData.player.global_position - global_position).normalized() # normalized() removes the distance part and just turns it into directions
 	velocity = direction * SPEED
-	rotation = velocity.angle()
 	move_and_slide()
+	
+	if direction.x > 0:
+		$Sprite2D.flip_h = true
+	elif direction.x < 0:
+		$Sprite2D.flip_h = false
+	
 
 func take_damage(amt):
 	HEALTH -= amt
